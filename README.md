e2194621913f7a6b24f42a8f1e8a3087d22767bc
Ing. Peter VOJTECH ml. - petak23 <petak23@gmail.com>
2020-12-09 07:21:08 +0100

    V 1.0.2 - úpravy a doplnenie dokumentácie

--- README.md
+++ README.md
@@ -2,6 +2,13 @@
 
 Ovládanie LEGO autíčka pomocou ESP8266.
 
+## Príkazy cez wifi
+
+Príkaz spolu s doplňujúcou informáciou v tvare *PPPxxx*, kde *PPP* - príkaz; *xxx* - doplňujúca informácia.
+
+Povolené príkazy z wifi:
+ - **moCxyz** - pohyb motora("mo"), kde C je číslo motora a xyz = 0..127 - vzad; 128 - stop; 129..255 - vpred
+
 ## Odkazy a referencie
 
  - [https://www.hwkitchen.cz/geekservo-9g-360-kontinualni-kompatibilni-s-lego/](https://www.hwkitchen.cz/geekservo-9g-360-kontinualni-kompatibilni-s-lego/)
--- src/main.cpp
+++ src/main.cpp
@@ -1,63 +1,59 @@
 /* 
  * Program pre ovládanie dvoch LEGO motorčekov pomocou ESP8266
  *
- * Posledna zmena(last change): 08.12.2020
+ * Posledna zmena(last change): 09.12.2020
  * @author Ing. Peter VOJTECH ml. <petak23@gmail.com>
  * @copyright  Copyright (c) 2016 - 2020 Ing. Peter VOJTECH ml.
  * @license
  * @link       http://petak23.echo-msz.eu
- * @version 1.0.1
+ * @version 1.0.2
  * 
- * Toto servo:
+ * Servo:
  * https://www.hwkitchen.cz/geekservo-9g-360-kontinualni-kompatibilni-s-lego/
  *
+ * Príkaz spolu s doplňujúcou informáciou v tvare PPPxxx, 
+ * kde PPP - príkaz; xxx - doplňujúca informácia
+ * 
+ * Povolené príkazy z wifi:
+ * moCxyz - pohyb motora, kde C- čísko motora a xyz = 0..127 - vzad; 128 - stop; 129..255 - vpred
  */
 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
 
-const int pwm1_pin = 12; // D6
-const int pwm2_pin = 13; // D7
-int rychlost1, rychlost2 = 0;
+const byte pwm_pins[2] = {12, 13}; // D6, D7
+
+int min_servo = 0;       // Minimálna hodnota pre servo
+int cen_servo = 512;     // Stredná    -- // --
+int max_servo = 1023;    // Maximálna  -- // --
 
 void setup() {
-  // put your setup code here, to run once:
   //Wifi.begin();
   WiFi.softAP("ESPsoftAP_auticko");
 }
 
 void loop() {
   if (Wifi.available()) {
-    String msg = Wifi.readStringUntil('\n'); // Príkaz spolu s doplňujúcou informáciou v tvare PPPxxxx, 
-                                             // kde PPP - príkaz; xxxx - doplňujúca informácia
-    String _tmpMsg =  msg.substring(3);      // Pomocná premenná pre doplňujúcu informáciu
-    /**
-     * Povolené príkazy z wifi:
-     * moCxyz - pohyb motora, kde C- čísko motora a xyz = 0..511 - vzad; 512 - stop; 513..1023 - vpred
-     */
-    if (msg.startsWith("mo1")) {
-      _tmpMsg = msg.substring(3);
-      rychlost1 = _tmpMsg.toInt();
-      Wifi.println(String("Motor1: \"")  + String(_tmpMsg) + String("->") + String(rychlost1) + String("\" ") + String(msg));
-      if (rychlost1 > 512) {        // Pohyb motora vpred
-        analogWrite(pwm1_pin, rychlost1);
-      } else if (rychlost1 < 512) { // Pohyb motora vzad
-        analogWrite(pwm1_pin, rychlost1);
-      } else {                     // Motor stop
-        analogWrite(pwm1_pin, 0);
+    String msg = Wifi.readStringUntil('\n');
+    if (msg.startsWith("mo")) {              // Zisti, či je príkaz pre motor
+      String _tmpRy = msg.substring(3);      // Zisti rýchlosť
+      int rychlost = _tmpRy.toInt();
+
+      String _tmpId = msg.substring(2, 3);   // Zisti id motora
+      int id = _tmpId.toInt();
+
+      if ((id == 1) || (id == 2)) {   // Over či id = 1 alebo 2
+        int servo_rychlost = map(rychlost, 0, 255, min_servo, max_servo);
+        Wifi.println(String("Motor1: \"")  + String(id) + String("->") + String(rychlost) + String("\" ") + String(msg));
+        analogWrite(pwm_pins[(id-1)], servo_rychlost);
       }
-    }
-    if (msg.startsWith("mo2")) {
-      _tmpMsg = msg.substring(3);
-      rychlost2 = _tmpMsg.toInt();
-      Wifi.println(String("Motor2: \"")  + String(_tmpMsg) + String("->") + String(rychlost2) + String("\" ") + String(msg));
-      if (rychlost2 > 512) {        // Pohyb motora vpred
-        analogWrite(pwm2_pin, rychlost2);
-      } else if (rychlost2 < 512) { // Pohyb motora vzad
-        analogWrite(pwm2_pin, rychlost2);
-      } else {                     // Motor stop
-        analogWrite(pwm2_pin, 0);
-      }
+      /*if (rychlost1 > 128) {        // Pohyb motora vpred
+        analogWrite(pwm1_pin, servo_rychlost);
+      } else if (rychlost1 < 128) { // Pohyb motora vzad
+        analogWrite(pwm1_pin, servo_rychlost);
+      } else {                      // Motor stop
+        analogWrite(pwm1_pin, servo_rychlost);
+      }*/
     }
   }
 }
