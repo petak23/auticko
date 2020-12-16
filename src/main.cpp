3993ae8038c4ccfc0f21df5c6e15cdc3c88c83ac
Ing. Peter VOJTECH ml. - petak23 <petak23@gmail.com>
2020-12-16 05:58:27 +0100

    V 1.0.3 - aktualizácia main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 /* 
  * Program pre ovládanie dvoch LEGO motorčekov pomocou ESP8266
  *
- * Posledna zmena(last change): 09.12.2020
+ * Posledna zmena(last change): 16.12.2020
  * @author Ing. Peter VOJTECH ml. <petak23@gmail.com>
  * @copyright  Copyright (c) 2016 - 2020 Ing. Peter VOJTECH ml.
  * @license
  * @link       http://petak23.echo-msz.eu
- * @version 1.0.2
+ * @version 1.0.3
  * 
  * Servo:
  * https://www.hwkitchen.cz/geekservo-9g-360-kontinualni-kompatibilni-s-lego/
@@ -20,6 +20,15 @@
 
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
+//#include <WiFi.h>
+
+IPAddress local_IP(192,168,4,200);
+IPAddress gateway(192,168,4,100);
+IPAddress subnet(255,255,255,0);
+
+const char* host     = "auticko";
+
+WiFiClient client;
 
 const byte pwm_pins[2] = {12, 13}; // D6, D7
 
@@ -28,13 +37,31 @@ int cen_servo = 512;     // Stredná    -- // --
 int max_servo = 1023;    // Maximálna  -- // --
 
 void setup() {
+  Serial.begin(9600);
   //Wifi.begin();
-  WiFi.softAP("ESPsoftAP_auticko");
+  Serial.print("Nastavujem soft-AP konfiguráciu ... ");
+  Serial.println(WiFi.softAPConfig(local_IP, gateway, subnet));
+
+  Serial.print("Nastavujem soft-AP pripojenie ...");
+  Serial.println(WiFi.softAP("ESPsoftAP_auticko") ? "Ready" : "Failed!");
+  
+  Serial.print("Moja IP adresa je:");
+  Serial.println(WiFi.softAPIP());
+
+  // Use WiFiClient class to create TCP connections
+  const int httpPort = 80;
+  if (!client.connect(host, httpPort)) {
+    Serial.println("connection failed");
+    return;
+  } else {
+    Serial.print("Host "); Serial.print(host);
+    Serial.println(" pripojený");
+  }
 }
 
 void loop() {
-  if (Wifi.available()) {
-    String msg = Wifi.readStringUntil('\n');
+  if (client.available()) {
+    String msg = client.readStringUntil('\n');
     if (msg.startsWith("mo")) {              // Zisti, či je príkaz pre motor
       String _tmpRy = msg.substring(3);      // Zisti rýchlosť
       int rychlost = _tmpRy.toInt();
@@ -44,7 +71,7 @@ void loop() {
 
       if ((id == 1) || (id == 2)) {   // Over či id = 1 alebo 2
         int servo_rychlost = map(rychlost, 0, 255, min_servo, max_servo);
-        Wifi.println(String("Motor1: \"")  + String(id) + String("->") + String(rychlost) + String("\" ") + String(msg));
+        client.println(String("Motor1: \"")  + String(id) + String("->") + String(rychlost) + String("\" ") + String(msg));
         analogWrite(pwm_pins[(id-1)], servo_rychlost);
       }
       /*if (rychlost1 > 128) {        // Pohyb motora vpred
