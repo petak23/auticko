e8ddd3c62b46012c83937f378776e83051911c24
petak23 <petak23@users.noreply.github.com>
2020-12-28 12:19:01 +0100

    V 1.0.6 - úpravy v nastavení pwm

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,12 @@
 /* 
  * Program pre ovládanie dvoch LEGO motorčekov pomocou ESP8266
  *
- * Posledna zmena(last change): 26.12.2020
+ * Posledna zmena(last change): 28.12.2020
  * @author Ing. Peter VOJTECH ml. <petak23@gmail.com>
  * @copyright  Copyright (c) 2016 - 2020 Ing. Peter VOJTECH ml.
  * @license
  * @link       http://petak23.echo-msz.eu
- * @version 1.0.5
+ * @version 1.0.6
  * 
  * Servo:
  * https://www.hwkitchen.cz/geekservo-9g-360-kontinualni-kompatibilni-s-lego/
@@ -35,10 +35,13 @@ WiFiServer server(port);
 WiFiClient client;
 
 const byte pwm_pins[2] = {12, 13}; // D6, D7
-
-int min_servo = 0;       // Minimálna hodnota pre servo
-int cen_servo = 512;     // Stredná    -- // --
-int max_servo = 1023;    // Maximálna  -- // --
+/* Servo sa zastaví pri nastavení 90°.
+ * Zadaním menšej hodnoty ako 90° sa servo roztočí na jednu stranu
+ * a zadaním větší hodnoty na druhú.
+ * Čím viac se hodnota blíži 180° alebo 0°, tým väčšie sú otáčky serva. */
+int min_servo = 600;      // Minimálna hodnota pre servo
+int cen_servo = 1500;     // Stredná    -- // --         - STOP
+int max_servo = 2400;     // Maximálna  -- // --
 
 char cmd[100]; // stores the command chars received from RoboRemo
 int cmdIndex;
@@ -65,7 +68,12 @@ void setup() {
   Serial.println((String)"RoboRemo sa musí pripojiť na: " + my_IP.toString() + ":" + port);
 
   pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);  
+  digitalWrite(LED_BUILTIN, LOW); 
+  
+  // Nastavenie 
+  analogWrite(pwm_pins[0], cen_servo); //Stop
+  analogWrite(pwm_pins[1], cen_servo);
+  
 }
 
 void loop() {
@@ -88,13 +96,6 @@ void loop() {
         client.println(String("Motor1: \"")  + String(id) + String("->") + String(rychlost) + String("\" ") + String(msg));
         analogWrite(pwm_pins[(id-1)], servo_rychlost);
       }
-      /*if (rychlost1 > 128) {        // Pohyb motora vpred
-        analogWrite(pwm1_pin, servo_rychlost);
-      } else if (rychlost1 < 128) { // Pohyb motora vzad
-        analogWrite(pwm1_pin, servo_rychlost);
-      } else {                      // Motor stop
-        analogWrite(pwm1_pin, servo_rychlost);
-      }*/
     } else if (msg.startsWith("led")) {   
       String _tmpLED = msg.substring(3, 4);
       digitalWrite(LED_BUILTIN, _tmpLED.toInt());
